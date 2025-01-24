93041e89fccf9b2dffc289a9f458824101510a81
Reshiro <michaelcheong7@gmail.com>
2025-01-24 16:18:54 +0800

    Add support for echo

--- src/main/java/YoChan.java
+++ src/main/java/YoChan.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class YoChan {
     public static void main(String[] args) {
-        System.out.println("Ough... Hello! I'm Yo-Chan!");
-        System.out.println("Bye bye!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ough... What do you want?");
+
+        // Infinite loop until user inputs "bye"
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                System.out.println("-*-*-*-*-");
+                System.out.println("Bye bye!");
+                System.out.println("-*-*-*-*-");
+                break;
+            }
+            // Echoes user input
+            System.out.println("-*-*-*-*-");
+            System.out.println(userInput);
+            System.out.println("-*-*-*-*-");
+        }
+
+        scanner.close();
     }
 }
