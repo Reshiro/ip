f827878278799e948b17825e631b4139eee80dda
Reshiro <michaelcheong7@gmail.com>
2025-01-24 19:34:56 +0800

    Add marking and unmarking of tasks

--- src/main/java/YoChan.java
+++ src/main/java/YoChan.java
@@ -2,23 +2,21 @@ import java.util.Scanner;
 
 public class YoChan {
     public static void main(String[] args) {
-
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ough... What do you want?");
 
-        // Infinite loop until user inputs "bye"
         while (true) {
             String userInput = scanner.nextLine();
+
             if (userInput.equals("bye")) {
                 System.out.println("-*-*-*-*-");
                 System.out.println("Bye bye!");
                 System.out.println("-*-*-*-*-");
                 break;
 
-            // Lists all tasks inputted by user
             } else if (userInput.equals("list")) {
                 System.out.println("-*-*-*-*-");
                 for (int i = 0; i < taskCount; i++) {
@@ -26,9 +24,48 @@ public class YoChan {
                 }
                 System.out.println("-*-*-*-*-");
 
-            // Adds inputted task to list of tasks
+            } else if (userInput.startsWith("mark")) {
+                try {
+                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                    if (taskNumber > 0 && taskNumber <= taskCount) {
+                        tasks[taskNumber - 1].markAsDone();
+                        System.out.println("-*-*-*-*-");
+                        System.out.println("Ough! I've marked this task as done:");
+                        System.out.println(tasks[taskNumber - 1]);
+                        System.out.println("-*-*-*-*-");
+                    } else {
+                        System.out.println("-*-*-*-*-");
+                        System.out.println("Invalid task number!");
+                        System.out.println("-*-*-*-*-");
+                    }
+                } catch (Exception e) {
+                    System.out.println("-*-*-*-*-");
+                    System.out.println("Please specify a valid task number after 'mark'!");
+                    System.out.println("-*-*-*-*-");
+                }
+
+            } else if (userInput.startsWith("unmark")) {
+                try {
+                    int taskNumber = Integer.parseInt(userInput.split(" ")[1]);
+                    if (taskNumber > 0 && taskNumber <= taskCount) {
+                        tasks[taskNumber - 1].unmarkAsDone();
+                        System.out.println("-*-*-*-*-");
+                        System.out.println("Ough... I've marked this task as not done yet:");
+                        System.out.println(tasks[taskNumber - 1]);
+                        System.out.println("-*-*-*-*-");
+                    } else {
+                        System.out.println("-*-*-*-*-");
+                        System.out.println("Invalid task number!");
+                        System.out.println("-*-*-*-*-");
+                    }
+                } catch (Exception e) {
+                    System.out.println("-*-*-*-*-");
+                    System.out.println("Please specify a valid task number after 'unmark'!");
+                    System.out.println("-*-*-*-*-");
+                }
+
             } else {
-                tasks[taskCount] = userInput;
+                tasks[taskCount] = new Task(userInput);
                 taskCount++;
                 System.out.println("-*-*-*-*-");
                 System.out.println("added: " + userInput);
