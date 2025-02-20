e43a8c67730bd4798e3035bd96e41703a4a08c20
Reshiro <michaelcheong7@gmail.com>
2025-02-21 07:11:10 +0800

    Add chatbot name to the titlebar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("YoChan");
             fxmlLoader.<MainWindow>getController().setYoChan(yoChan);
             stage.show();
         } catch (IOException e) {
